ee445bf4fb8afe3d8bfb7d4c48f5583cc2338004
Ashika Shallow <slwashika@gmail.com>
2020-08-12 14:43:41 -0400

    Added the reference variables

--- src/retailpricecalculator/RetailPriceCalculator.java
+++ src/retailpricecalculator/RetailPriceCalculator.java
@@ -1,12 +1,27 @@
 
 package retailpricecalculator;
 
+import javax.swing.*;
+
 /**
  *  This program allows users to enter the cost and markup of an item, then
  * displays the retail price.
  * @author Ashika Shallow
  */
-public class RetailPriceCalculator {
+public class RetailPriceCalculator extends JFrame{
+    
+    private JLabel wholesale;
+    private JLabel markup;
+    private JLabel retail;
+    JTextField wholesaleTxt;
+    JTextField markupTxt;
+    JButton calcButton;
+    JPanel panel;
+    
+    private double wholesalePrice;
+    private double markupCost;
+    
+    
 
     /**
      * @param args the command line arguments
