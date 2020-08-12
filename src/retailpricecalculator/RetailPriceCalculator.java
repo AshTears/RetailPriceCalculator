47115ec534eba7d5649413431d11bc3994476a29
Ashika Shallow <slwashika@gmail.com>
2020-08-12 14:40:28 -0400

    Initial commit

---
+++ src/retailpricecalculator/RetailPriceCalculator.java
@@ -0,0 +1,18 @@
+
+package retailpricecalculator;
+
+/**
+ *  This program allows users to enter the cost and markup of an item, then
+ * displays the retail price.
+ * @author Ashika Shallow
+ */
+public class RetailPriceCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
